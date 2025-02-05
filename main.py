d5f3b76c4cbc0867f0f44646a0259af95016c08d
Diikura <yuzu.daiki@gmail.com>
2025-02-05 21:54:08 +0900

    feature/matplotlib削除

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import yfinance as yf
 
 from get_finance import get_finance_df
